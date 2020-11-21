548a869a57863406574365937fb21a2e496e06da
Lee-Roy King <leeroywking@gmail.com>
2020-11-21 14:04:45 -0800

    added lscpu info

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import speedtest
-import platform, socket, re, uuid, json, logging, subprocess, glob, shutil, psutil
+import platform, socket, re, uuid, json, logging, subprocess, glob, shutil
 
 
 def getSystemInfo():
@@ -10,7 +10,7 @@ def getSystemInfo():
         info["platform-version"] = platform.version()
         info["architecture"] = platform.machine()
         info["total_harddrive_space"] = shutil.disk_usage("/")[0]
-        info["total_memory"] = psutil.virtual_memory()[0]
+        info["processor"] = platform.processor()
         # info
         # ["hostname"] = socket.gethostname()
         # info["ip-address"] = socket.gethostbyname(socket.gethostname())
@@ -20,6 +20,19 @@ def getSystemInfo():
     except Exception as e:
         logging.exception(e)
 
+def get_cpu_info():
+    info = {"extras":[]}
+    cpu_info = subprocess.run("lscpu", stdout=subprocess.PIPE).stdout.decode("utf-8")
+    cpu_info = cpu_info.split("\n")
+    for line in cpu_info:
+        try:
+            pair = line.split(":")
+            left = pair[0].strip()
+            right = pair[1].strip()
+            info[left] = right
+        except:
+            info["extras"].append(line)
+    return info
 
 def get_release_info():
     release_info = subprocess.run(
@@ -40,10 +53,12 @@ def get_release_info():
 if __name__ == "__main__":
     system_info = json.loads(getSystemInfo())
     release_info = get_release_info()
+    cpu_info = get_cpu_info()
     # speedtest_results = speedtest.shell()
     output = {
         "system_info": system_info,
         "linux_info": release_info,
         # "speedtest_info": speedtest_results.dict(),
+        "cpu_info" : cpu_info
     }
     print(json.dumps(output, indent=4))
