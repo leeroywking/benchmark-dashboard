6489d376970628d185e952eb51cb210e9cb65885
Lee-Roy King <leeroywking@gmail.com>
2020-11-22 15:43:01 -0800

    downgrade to python 3.6

--- helpers/setup.sh
+++ helpers/setup.sh
@@ -1,2 +1,9 @@
-# Box will need python3.8^
-# poetry
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install python3 git -y
+sudo pip3 install poetry
+
+git clone https://github.com/leeroywking/benchmark-dashboard.git
+cd benchmark-dashboard
+poetry install
---
+++ venv/cpython3.8-6c9a7ee644b4/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lee/projects/benchmark-dashboard/venv/cpython3.8-6c9a7ee644b4/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
